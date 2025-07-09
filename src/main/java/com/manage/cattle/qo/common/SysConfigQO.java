e79a8732d263038c18bb54986c0329b38072b780
wujinyi123 <996019864@qq.com>
2025-07-10 00:05:59 +0800

    init

--- src/main/java/com/manage/cattle/qo/common/SysConfigQO.java
+++ src/main/java/com/manage/cattle/qo/common/SysConfigQO.java
@@ -2,9 +2,11 @@ package com.manage.cattle.qo.common;
 
 import com.manage.cattle.qo.PageQO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class SysConfigQO extends PageQO {
     private String code;
